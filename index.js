b4ff95654544da6ba414bde9e626889c39bd6f2b
collin254 <cmwenda20@gmail.com>
2024-10-11 19:40:17 +0300

    again

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -19,9 +21,7 @@ const mongoUri = process.env.MONGO_URI || 'mongodb+srv://gci:Collin@gci.fc2y1.mo
 // Middleware
 app.use(cors({
   credentials: true,
-  origin: process.env.NODE_ENV === 'production' ? 'https://gci-kutus-web.vercel.app' : 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allowing all necessary methods
-  allowedHeaders: ['Content-Type', 'Authorization'],  // Allow headers needed for requests
+  origin: process.env.NODE_ENV === 'production' ? 'https://gci-kutus-web.vercel.app' : 'http://localhost:3000'
 }));
 app.use(express.json());
 app.use(cookieParser());
@@ -63,7 +63,7 @@ app.post('/login', async (req, res) => {
       res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',  // Secure cookie for production
-        sameSite: 'None',  // Allow cross-origin requests
+        sameSite: 'None',  // To allow cross-origin requests
       }).json({
         id: userDoc._id,
         username,
@@ -167,174 +167,6 @@ app.get('/post/:id', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-// const express = require('express');
-// const cors = require('cors');
-// const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
-// const multer = require('multer');
-// const fs = require('fs');
-// const cookieParser = require('cookie-parser');
-// const User = require('./models/User');
-// const Post = require('./models/Post');
-// const app = express();
-
-// // Constants from environment variables
-// const salt = bcrypt.genSaltSync(10);
-// const secret = process.env.JWT_SECRET || 'asdfe45we45w345wegw345werjktjwertkj'; // Using environment variable for JWT secret
-// const port = process.env.PORT || 4000; // Dynamic port for Heroku
-// const mongoUri = process.env.MONGO_URI || 'mongodb+srv://gci:Collin@gci.fc2y1.mongodb.net/?retryWrites=true&w=majority&appName=gci'; // Mongo URI from environment variables
-
-// // Middleware
-// app.use(cors({
-//   credentials: true,
-//   origin: process.env.NODE_ENV === 'production' ? 'https://gci-kutus-web.vercel.app' : 'http://localhost:3000'
-// }));
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use('/uploads', express.static(__dirname + '/uploads'));
-
-// // MongoDB connection
-// mongoose.connect(mongoUri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then(() => console.log('MongoDB connected'))
-//   .catch(err => console.error('MongoDB connection error:', err));
-
-// // File Upload Middleware
-// const uploadMiddleware = multer({ dest: 'uploads/' });
-
-// // Register Route
-// app.post('/register', async (req, res) => {
-//   const { username, password } = req.body;
-//   try {
-//     const userDoc = await User.create({
-//       username,
-//       password: bcrypt.hashSync(password, salt),
-//     });
-//     res.json(userDoc);
-//   } catch (e) {
-//     console.error(e);
-//     res.status(400).json(e);
-//   }
-// });
-
-// // Login Route
-// app.post('/login', async (req, res) => {
-//   const { username, password } = req.body;
-//   const userDoc = await User.findOne({ username });
-//   const passOk = bcrypt.compareSync(password, userDoc.password);
-//   if (passOk) {
-//     jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
-//       if (err) throw err;
-//       res.cookie('token', token, {
-//         httpOnly: true,
-//         secure: process.env.NODE_ENV === 'production',  // Secure cookie for production
-//         sameSite: 'None',  // To allow cross-origin requests
-//       }).json({
-//         id: userDoc._id,
-//         username,
-//       });
-//     });
-//   } else {
-//     res.status(400).json('wrong credentials');
-//   }
-// });
-
-// // Profile Route
-// app.get('/profile', (req, res) => {
-//   const { token } = req.cookies;
-//   jwt.verify(token, secret, {}, (err, info) => {
-//     if (err) throw err;
-//     res.json(info);
-//   });
-// });
-
-// // Logout Route
-// app.post('/logout', (req, res) => {
-//   res.cookie('token', '', {
-//     httpOnly: true,
-//     secure: process.env.NODE_ENV === 'production',
-//     sameSite: 'None',
-//   }).json('ok');
-// });
-
-// // Create Post Route
-// app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
-//   const { originalname, path } = req.file;
-//   const parts = originalname.split('.');
-//   const ext = parts[parts.length - 1];
-//   const newPath = path + '.' + ext;
-//   fs.renameSync(path, newPath);
-
-//   const { token } = req.cookies;
-//   jwt.verify(token, secret, {}, async (err, info) => {
-//     if (err) throw err;
-//     const { title, summary, content } = req.body;
-//     const postDoc = await Post.create({
-//       title,
-//       summary,
-//       content,
-//       cover: newPath,
-//       author: info.id,
-//     });
-//     res.json(postDoc);
-//   });
-// });
-
-// // Update Post Route
-// app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
-//   let newPath = null;
-//   if (req.file) {
-//     const { originalname, path } = req.file;
-//     const parts = originalname.split('.');
-//     const ext = parts[parts.length - 1];
-//     newPath = path + '.' + ext;
-//     fs.renameSync(path, newPath);
-//   }
-
-//   const { token } = req.cookies;
-//   jwt.verify(token, secret, {}, async (err, info) => {
-//     if (err) throw err;
-//     const { id, title, summary, content } = req.body;
-//     const postDoc = await Post.findById(id);
-//     const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
-//     if (!isAuthor) {
-//       return res.status(400).json('you are not the author');
-//     }
-//     await postDoc.update({
-//       title,
-//       summary,
-//       content,
-//       cover: newPath ? newPath : postDoc.cover,
-//     });
-
-//     res.json(postDoc);
-//   });
-// });
-
-// // Get All Posts Route
-// app.get('/post', async (req, res) => {
-//   res.json(
-//     await Post.find()
-//       .populate('author', ['username'])
-//       .sort({ createdAt: -1 })
-//       .limit(20)
-//   );
-// });
-
-// // Get Single Post by ID
-// app.get('/post/:id', async (req, res) => {
-//   const { id } = req.params;
-//   const postDoc = await Post.findById(id).populate('author', ['username']);
-//   res.json(postDoc);
-// });
-
-// // Start the Server
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
 
 
 // const express = require('express');
